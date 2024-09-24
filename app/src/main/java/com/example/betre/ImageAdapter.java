704b03e9b7b5ce0db6c2af8dfb8184244ce2adf3
pathaniamohit <pathaniamohit0711raj@gmail.com>
2024-09-24 18:01:25 -0400

    ImageAdapter.java and item_image.xml added

---
+++ app/src/main/java/com/example/betre/ImageAdapter.java
@@ -0,0 +1,56 @@
+package com.example.betre;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.betre.R;
+
+import java.util.List;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+
+    private Context context;
+    private List<String> imageUrls;
+
+    public ImageAdapter(Context context, List<String> imageUrls) {
+        this.context = context;
+        this.imageUrls = imageUrls;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_image, parent, false);
+        return new ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        String imageUrl = imageUrls.get(position);
+        Glide.with(context)
+                .load(imageUrl)
+                .placeholder(R.drawable.ic_profile_placeholder)
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageUrls.size();
+    }
+
+    public static class ImageViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image_item);
+        }
+    }
+}
