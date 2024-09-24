3e2675b9128c53d713c3dacf2a94f7d43f5245f9
asrjashan <jashanchhapa85@gmail.com>
2024-09-24 11:03:23 -0400

    signup link

--- app/src/main/java/com/example/betre/LoginActivity.java
+++ app/src/main/java/com/example/betre/LoginActivity.java
@@ -19,13 +19,24 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-
+//Intialize view
         username = findViewById(R.id.usernamelogin);
         loginpassword = findViewById(R.id.passwordlogin);
         forgotpassword = findViewById(R.id.forgot_password);
         loginbtn = findViewById(R.id.login);
         signuplink = findViewById(R.id.signuplink);
         mAuth = com.google.firebase.auth.FirebaseAuth.getInstance();
+
+        //signup link
+        signuplink.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                android.content.Intent intent = new android.content.Intent(LoginActivity.this, SignupPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
 
     }
 }
