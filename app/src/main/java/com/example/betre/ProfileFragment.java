fb74e73f2afaa44eced3cdd24abb8f448f7aa1e7
pathaniamohit <pathaniamohit0711raj@gmail.com>
2024-09-24 18:24:46 -0400

    ProfileFragment.java updated (getUserImages() function added)

--- app/src/main/java/com/example/betre/ProfileFragment.java
+++ app/src/main/java/com/example/betre/ProfileFragment.java
@@ -1,16 +1,22 @@
 package com.example.betre;
 
-import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,10 +28,9 @@ public class ProfileFragment extends Fragment {
 
     private RecyclerView imagesRecyclerView;
     private ImageAdapter imageAdapter;
-
+    private List<String> imageUrls = new ArrayList<>();
 
     // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -45,7 +50,6 @@ public class ProfileFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment ProfileFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
@@ -71,12 +75,35 @@ public class ProfileFragment extends Fragment {
 
         imagesRecyclerView = view.findViewById(R.id.images_grid);
         imagesRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
-        imageAdapter = new ImageAdapter(getContext(), getUserImages());
+
+        imageAdapter = new ImageAdapter(getContext(), imageUrls);
         imagesRecyclerView.setAdapter(imageAdapter);
+
+        getUserImages();
 
         return view;
     }
 
-    private List<Image> getUserImages() {
+    private void getUserImages() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        CollectionReference imagesRef = db.collection("user_images");
+
+        imagesRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                QuerySnapshot documents = task.getResult();
+                if (documents != null) {
+                    for (DocumentSnapshot document : documents) {
+                        String url = document.getString("url");
+                        if (url != null) {
+                            imageUrls.add(url);
+                        }
+                    }
+                    imageAdapter.notifyDataSetChanged();
+                }
+            } else {
+                Log.e("ProfileFragment", "Error fetching images", task.getException());
+            }
+        });
     }
 }
