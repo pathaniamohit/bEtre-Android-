c8d5b7c76fef5a889f49022f40c74d9fad9cfba4
asrjashan <jashanchhapa85@gmail.com>
2024-09-23 22:32:57 -0400

    SplashScreen

---
+++ app/src/main/java/com/example/betre/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.example.betre;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final int DELAY = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        android.os.Handler handler =  new android.os.Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                android.content.Intent intent =new android.content.Intent(SplashScreen.this,LoginPage.class);
+                startActivity(intent);
+            }
+        },DELAY);
+    }
+}
