de6aa0c045419e4535c6029c8c2cb44f99b13cd3
asrjashan <jashanchhapa85@gmail.com>
2024-09-25 19:04:28 -0400

    user class

---
+++ app/src/main/java/com/example/betre/User.java
@@ -0,0 +1,17 @@
+package com.example.betre;
+
+public class User {
+    public String name;
+    public String email;
+    public String username;
+
+    public User() {
+
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+}
