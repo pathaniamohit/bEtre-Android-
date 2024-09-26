940900ce5cd4963ea45ff90511f5999dd10ee57b
asrjashan <jashanchhapa85@gmail.com>
2024-09-26 12:17:57 -0400

    user class

--- app/src/main/java/com/example/betre/User.java
+++ app/src/main/java/com/example/betre/User.java
@@ -3,15 +3,18 @@ package com.example.betre;
 public class User {
     public String name;
     public String email;
-    public String username;
+    public String phone;
+    public String gender;
 
     public User() {
-
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public User(String name, String email) {
+    public User(String name, String email, String phone, String gender) { // Updated constructor
         this.name = name;
         this.email = email;
+        this.phone = phone;
+        this.gender = gender;
     }
-
 }
+
