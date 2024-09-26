0c0b73da781d2472d33cc6f126616b32d4fd862e
Your Github Username <your-singhamritpal1919@gmail.com>
2024-09-26 18:56:04 -0400

    Your commit message here

--- app/src/main/java/com/example/betre/LoginActivity.java
+++ app/src/main/java/com/example/betre/LoginActivity.java
@@ -35,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
