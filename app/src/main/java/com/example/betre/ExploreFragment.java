eabb1f76323e064828bf8cfe470855b60aa936c0
Your Github Username <your-singhamritpal1919@gmail.com>
2024-09-28 21:48:30 -0400

    Explore Fragment

---
+++ app/src/main/java/com/example/betre/ExploreFragment.java
@@ -0,0 +1,23 @@
+package com.example.betre;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+public class ExploreFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_explore_fragment, container, false);
+    }
+}
