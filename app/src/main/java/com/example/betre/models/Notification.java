80e2e84a9301ac15af47f6307e57cc6541dda43d
pathaniamohit <pathaniamohit0711raj@gmail.com>
2024-10-15 23:44:08 -0400

    notifications 1.3 (model class)

---
+++ app/src/main/java/com/example/betre/models/Notification.java
@@ -0,0 +1,61 @@
+package com.example.betre.models;
+
+public class Notification {
+    private String postId;
+    private String type;
+    private String username;
+    private String content;
+    private long timestamp;
+
+    public Notification() {
+        // Default constructor required for Firebase
+    }
+
+    public Notification(String postId, String type, String username, String content, long timestamp) {
+        this.type = type;
+        this.postId = postId;
+        this.username = username;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
